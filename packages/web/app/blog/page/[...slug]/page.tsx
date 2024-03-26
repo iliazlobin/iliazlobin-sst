82575f69a90a418caa979814fb4543bf9ebda4a4
Ilia Zlobin <iliazlobin91@gmail.com>
2024-03-26 15:17:54 -0400

    refactor into blog post component

--- packages/web/app/blog/page/[...slug]/page.tsx
+++ packages/web/app/blog/page/[...slug]/page.tsx
@@ -3,28 +3,17 @@ import Image from 'next/image'
 import { notFound } from 'next/navigation'
 
 import Tag from '@/components/Tag'
-import { CodeBlockWrapper } from '@/components/code-block-wrapper'
-import { CopyButton, CopyNpmCommandButton } from '@/components/copy-button'
-import { StyleWrapper } from '@/components/style-wrapper'
-import { Style } from '@/components/styles'
 import { DashboardTableOfContents } from '@/components/toc'
 import TocButton from '@/components/toc-button'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { siteConfig } from '@/config/site'
-import { Event } from '@/lib/events'
 import { getTableOfContents } from '@/lib/toc'
-import { cn } from '@/lib/utils'
 import { getWebPosts, retrieveWebPost } from '@/service/post'
 import '@/styles/mdx.css'
-import { NpmCommands } from '@/types/unist'
 
 import { MDXRemote } from 'next-mdx-remote/rsc'
-import { Suspense } from 'react'
-import rehypeSlug from 'rehype-slug'
-import remarkGfm from 'remark-gfm'
-import { visit } from 'unist-util-visit'
 
 // property og:site_name Notion
 // property og:type website
@@ -144,280 +133,16 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 }
 
-export const components = {
-  h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h1
-      className={cn(
-        'font-heading mt-2 scroll-m-20 text-4xl font-bold',
-        className,
-      )}
-      {...props}
-    />
-  ),
-  h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h2
-      className={cn(
-        'font-heading mt-12 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0',
-        className,
-      )}
-      {...props}
-    />
-  ),
-  h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h3
-      className={cn(
-        'font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight',
-        className,
-      )}
-      {...props}
-    />
-  ),
-  h4: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h4
-      className={cn(
-        'font-heading mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
-        className,
-      )}
-      {...props}
-    />
-  ),
-  h5: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h5
-      className={cn(
-        'mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
-        className,
-      )}
-      {...props}
-    />
-  ),
-  h6: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
-    <h6
-      className={cn(
-        'mt-8 scroll-m-20 text-base font-semibold tracking-tight',
-        className,
-      )}
-      {...props}
-    />
-  ),
-  a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
-    <a
-      className={cn('font-medium underline underline-offset-4', className)}
-      {...props}
-    />
-  ),
-  p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
-    <p
-      className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}
-      {...props}
-    />
-  ),
-  ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
-    <ul className={cn('my-6 ml-6 list-disc', className)} {...props} />
-  ),
-  ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
-    <ol className={cn('my-6 ml-6 list-decimal', className)} {...props} />
-  ),
-  li: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
-    <li className={cn('mt-2', className)} {...props} />
-  ),
-  blockquote: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
-    <blockquote
-      className={cn('mt-6 border-l-2 pl-6 italic', className)}
-      {...props}
-    />
-  ),
-  img: ({
-    className,
-    alt,
-    src,
-    width,
-    height,
-    ...props
-  }: React.ImgHTMLAttributes<HTMLImageElement>) => {
-    if (width && height) {
-      return (
-        <Image
-          src={src || ''}
-          alt={alt || ''}
-          width={Number(width)}
-          height={Number(height)}
-        />
-      )
-    }
-    return (
-      <img
-        className={cn('rounded-md', className)}
-        src={src}
-        alt={alt}
-        {...props}
-      />
-    )
-  },
-  hr: ({ ...props }: React.HTMLAttributes<HTMLHRElement>) => (
-    <hr className="my-4 md:my-8" {...props} />
-  ),
-  table: ({ className, ...props }: React.HTMLAttributes<HTMLTableElement>) => (
-    <div className="my-6 w-full overflow-y-auto">
-      <table className={cn('w-full', className)} {...props} />
-    </div>
-  ),
-  tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
-    <tr
-      className={cn('m-0 border-t p-0 even:bg-muted', className)}
-      {...props}
-    />
-  ),
-  th: ({ className, ...props }: React.HTMLAttributes<HTMLTableCellElement>) => (
-    <th
-      className={cn(
-        'border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right',
-        className,
-      )}
-      {...props}
-    />
-  ),
-  td: ({ className, ...props }: React.HTMLAttributes<HTMLTableCellElement>) => (
-    <td
-      className={cn(
-        'border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right',
-        className,
-      )}
-      {...props}
-    />
-  ),
-  pre: ({
-    className,
-    __rawString__,
-    __npmCommand__,
-    __yarnCommand__,
-    __pnpmCommand__,
-    __bunCommand__,
-    __withMeta__,
-    __src__,
-    __event__,
-    __style__,
-    ...props
-  }: React.HTMLAttributes<HTMLPreElement> & {
-    __style__?: Style['name']
-    __rawString__?: string
-    __withMeta__?: boolean
-    __src__?: string
-    __event__?: Event['name']
-  } & NpmCommands) => {
-    return (
-      <StyleWrapper styleName={__style__}>
-        <pre
-          className={cn(
-            'mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border bg-zinc-950 py-4 dark:bg-zinc-900',
-            className,
-          )}
-          {...props}
-        />
-        {__rawString__ && !__npmCommand__ && (
-          <CopyButton
-            value={__rawString__}
-            src={__src__}
-            event={__event__}
-            className={cn('absolute right-4 top-4', __withMeta__ && 'top-16')}
-          />
-        )}
-        {__npmCommand__ &&
-          __yarnCommand__ &&
-          __pnpmCommand__ &&
-          __bunCommand__ && (
-            <CopyNpmCommandButton
-              commands={{
-                __npmCommand__,
-                __yarnCommand__,
-                __pnpmCommand__,
-                __bunCommand__,
-              }}
-              className={cn('absolute right-4 top-4', __withMeta__ && 'top-16')}
-            />
-          )}
-      </StyleWrapper>
-    )
-  },
-  code: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
-    <code
-      className={cn(
-        'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm',
-        className,
-      )}
-      {...props}
-    />
-  ),
-  Image,
-  CodeBlockWrapper: ({ ...props }) => (
-    <CodeBlockWrapper className="rounded-md border" {...props} />
-  ),
-  Step: ({ className, ...props }: React.ComponentProps<'h3'>) => (
-    <h3
-      className={cn(
-        'font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight',
-        className,
-      )}
-      {...props}
-    />
-  ),
-  Steps: ({ ...props }) => (
-    <div
-      className="[&>h3]:step steps mb-12 ml-4 border-l pl-8 [counter-reset:step]"
-      {...props}
-    />
-  ),
-}
-
 export default async function BlogPage({ params }: Props) {
   const slug = params.slug.join('/')
   const post = await retrieveWebPost({ slug })
 
-  if (!post) {
+  if (!post || !post.contentMd) {
     notFound()
   }
 
-  const contentMd = post.contentMd || ''
-  // const mdxObject = await compile(contentMd, { remarkPlugins: [remarkGfm] })
-
-  const options = {
-    parseFrontmatter: false,
-    mdxOptions: {
-      remarkPlugins: [remarkGfm],
-      rehypePlugins: [
-        rehypeSlug,
-        () => (tree: any) => {
-          visit(tree, node => {
-            if (node?.type === 'element' && node?.tagName === 'img') {
-              // const alt = node?.properties?.alt
-              const allMatch = node?.properties?.src.match(
-                /.*\/([^-]+?)-(\d+?)x(\d+?)\.(\w+?)$/,
-              )
-              if (allMatch) {
-                const name = allMatch[1]
-                const width = allMatch[2]
-                const height = allMatch[3]
-                const ext = allMatch[4]
-                if (!name || !width || !height || !ext) {
-                  return
-                }
-                node.properties = {
-                  ...node.properties,
-                  name,
-                  width,
-                  height,
-                  ext,
-                }
-                console.debug('DEBUG')
-              }
-            }
-          })
-        },
-      ],
-    },
-  }
-
-  const toc = await getTableOfContents(contentMd)
-  console.debug('DEBUG')
+  const content = post.contentMd
+  const toc = await getTableOfContents(content)
 
   return (
     <main className="container grid lg:grid-cols-[320px_auto] xl:grid-cols-[320px_auto_280px] gap-4 p-0">
@@ -491,11 +216,7 @@ export default async function BlogPage({ params }: Props) {
             />
           )}
         </div>
-        <MDXRemote
-          source={contentMd}
-          options={options}
-          components={components}
-        />
+        <MDXRemote source={content} />
       </div>
       <div className="hidden xl:block shrink-0 p-4">
         <div className="sticky top-20 text-sm">
---
+++ packages/web/components/blog-post-mdx.tsx
@@ -0,0 +1,280 @@
+import Image from 'next/image'
+
+import { CodeBlockWrapper } from '@/components/code-block-wrapper'
+import { CopyButton, CopyNpmCommandButton } from '@/components/copy-button'
+import { StyleWrapper } from '@/components/style-wrapper'
+import { Style } from '@/components/styles'
+import { Event } from '@/lib/events'
+import { cn } from '@/lib/utils'
+import '@/styles/mdx.css'
+import { NpmCommands } from '@/types/unist'
+
+import { MDXRemote } from 'next-mdx-remote/rsc'
+import rehypeSlug from 'rehype-slug'
+import remarkGfm from 'remark-gfm'
+import { visit } from 'unist-util-visit'
+
+export const components = {
+  h1: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h1
+      className={cn(
+        'font-heading mt-2 scroll-m-20 text-4xl font-bold',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  h2: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h2
+      className={cn(
+        'font-heading mt-12 scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  h3: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h3
+      className={cn(
+        'font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  h4: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h4
+      className={cn(
+        'font-heading mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  h5: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h5
+      className={cn(
+        'mt-8 scroll-m-20 text-lg font-semibold tracking-tight',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  h6: ({ className, ...props }: React.HTMLAttributes<HTMLHeadingElement>) => (
+    <h6
+      className={cn(
+        'mt-8 scroll-m-20 text-base font-semibold tracking-tight',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  a: ({ className, ...props }: React.HTMLAttributes<HTMLAnchorElement>) => (
+    <a
+      className={cn('font-medium underline underline-offset-4', className)}
+      {...props}
+    />
+  ),
+  p: ({ className, ...props }: React.HTMLAttributes<HTMLParagraphElement>) => (
+    <p
+      className={cn('leading-7 [&:not(:first-child)]:mt-6', className)}
+      {...props}
+    />
+  ),
+  ul: ({ className, ...props }: React.HTMLAttributes<HTMLUListElement>) => (
+    <ul className={cn('my-6 ml-6 list-disc', className)} {...props} />
+  ),
+  ol: ({ className, ...props }: React.HTMLAttributes<HTMLOListElement>) => (
+    <ol className={cn('my-6 ml-6 list-decimal', className)} {...props} />
+  ),
+  li: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+    <li className={cn('mt-2', className)} {...props} />
+  ),
+  blockquote: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+    <blockquote
+      className={cn('mt-6 border-l-2 pl-6 italic', className)}
+      {...props}
+    />
+  ),
+  img: ({
+    className,
+    alt,
+    src,
+    width,
+    height,
+    ...props
+  }: React.ImgHTMLAttributes<HTMLImageElement>) => {
+    if (width && height) {
+      return (
+        <Image
+          src={src || ''}
+          alt={alt || ''}
+          width={Number(width)}
+          height={Number(height)}
+        />
+      )
+    }
+    return (
+      <img
+        className={cn('rounded-md', className)}
+        src={src}
+        alt={alt}
+        {...props}
+      />
+    )
+  },
+  hr: ({ ...props }: React.HTMLAttributes<HTMLHRElement>) => (
+    <hr className="my-4 md:my-8" {...props} />
+  ),
+  table: ({ className, ...props }: React.HTMLAttributes<HTMLTableElement>) => (
+    <div className="my-6 w-full overflow-y-auto">
+      <table className={cn('w-full', className)} {...props} />
+    </div>
+  ),
+  tr: ({ className, ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
+    <tr
+      className={cn('m-0 border-t p-0 even:bg-muted', className)}
+      {...props}
+    />
+  ),
+  th: ({ className, ...props }: React.HTMLAttributes<HTMLTableCellElement>) => (
+    <th
+      className={cn(
+        'border px-4 py-2 text-left font-bold [&[align=center]]:text-center [&[align=right]]:text-right',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  td: ({ className, ...props }: React.HTMLAttributes<HTMLTableCellElement>) => (
+    <td
+      className={cn(
+        'border px-4 py-2 text-left [&[align=center]]:text-center [&[align=right]]:text-right',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  pre: ({
+    className,
+    __rawString__,
+    __npmCommand__,
+    __yarnCommand__,
+    __pnpmCommand__,
+    __bunCommand__,
+    __withMeta__,
+    __src__,
+    __event__,
+    __style__,
+    ...props
+  }: React.HTMLAttributes<HTMLPreElement> & {
+    __style__?: Style['name']
+    __rawString__?: string
+    __withMeta__?: boolean
+    __src__?: string
+    __event__?: Event['name']
+  } & NpmCommands) => {
+    return (
+      <StyleWrapper styleName={__style__}>
+        <pre
+          className={cn(
+            'mb-4 mt-6 max-h-[650px] overflow-x-auto rounded-lg border bg-zinc-950 py-4 dark:bg-zinc-900',
+            className,
+          )}
+          {...props}
+        />
+        {__rawString__ && !__npmCommand__ && (
+          <CopyButton
+            value={__rawString__}
+            src={__src__}
+            event={__event__}
+            className={cn('absolute right-4 top-4', __withMeta__ && 'top-16')}
+          />
+        )}
+        {__npmCommand__ &&
+          __yarnCommand__ &&
+          __pnpmCommand__ &&
+          __bunCommand__ && (
+            <CopyNpmCommandButton
+              commands={{
+                __npmCommand__,
+                __yarnCommand__,
+                __pnpmCommand__,
+                __bunCommand__,
+              }}
+              className={cn('absolute right-4 top-4', __withMeta__ && 'top-16')}
+            />
+          )}
+      </StyleWrapper>
+    )
+  },
+  code: ({ className, ...props }: React.HTMLAttributes<HTMLElement>) => (
+    <code
+      className={cn(
+        'relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  Image,
+  CodeBlockWrapper: ({ ...props }) => (
+    <CodeBlockWrapper className="rounded-md border" {...props} />
+  ),
+  Step: ({ className, ...props }: React.ComponentProps<'h3'>) => (
+    <h3
+      className={cn(
+        'font-heading mt-8 scroll-m-20 text-xl font-semibold tracking-tight',
+        className,
+      )}
+      {...props}
+    />
+  ),
+  Steps: ({ ...props }) => (
+    <div
+      className="[&>h3]:step steps mb-12 ml-4 border-l pl-8 [counter-reset:step]"
+      {...props}
+    />
+  ),
+}
+
+export default async function BlogPost({ source }: { source: string }) {
+  const options = {
+    parseFrontmatter: false,
+    mdxOptions: {
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [
+        rehypeSlug,
+        () => (tree: any) => {
+          visit(tree, node => {
+            if (node?.type === 'element' && node?.tagName === 'img') {
+              // const alt = node?.properties?.alt
+              const allMatch = node?.properties?.src.match(
+                /.*\/([^-]+?)-(\d+?)x(\d+?)\.(\w+?)$/,
+              )
+              if (allMatch) {
+                const name = allMatch[1]
+                const width = allMatch[2]
+                const height = allMatch[3]
+                const ext = allMatch[4]
+                if (!name || !width || !height || !ext) {
+                  return
+                }
+                node.properties = {
+                  ...node.properties,
+                  name,
+                  width,
+                  height,
+                  ext,
+                }
+                console.debug('DEBUG')
+              }
+            }
+          })
+        },
+      ],
+    },
+  }
+
+  return <MDXRemote source={source} options={options} components={components} />
+}
